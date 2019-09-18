e3e80ea3c6395fc63a482b7df9423cec373725d3
satyam <satyamvns42@gmail.com>
2019-09-18 10:36:20 +0530

    first

--- ps1.py
+++ ps1.py
@@ -1,1 +1,14 @@
-
+a = input("Enter the bit stream : ")
+for char in a[0:1]:
+    x = int(char)
+    print(x)
+for char in a[1:]:
+    y = int(char)
+    x ^= y
+print(x)
+a += str(x)
+print(a)
+a = a.replace('010', '0100'
+a += '0101'
+print(a)
+s
